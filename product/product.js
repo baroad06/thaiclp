c15b97eee85a88d3529d278522c771c14b5cc726
nutmos <nuttapong_mos@hotmail.com>
2018-04-01 07:40:30 +0700

    fix bug

--- product/product.js
+++ product/product.js
@@ -21,7 +21,6 @@ router.get('/:product_cat', function(req,res) {
     var all_product = JSON.parse(fs.readFileSync('./product/data.json', 'utf8'));
     for (var product in all_product.subfolder) {
         if (all_product.subfolder[product].url === req.params.product_cat) {
-            console.log(all_product.subfolder[product].url)
             res.render('pages/product-by-cat.ejs', {productcat: all_product.subfolder[product]});
         }
     }
