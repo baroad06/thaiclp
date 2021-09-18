6d506f3267d6c37a9cb2a2b0109068ae1c3fbb0f
baroad <boat.supawats@gmail.com>
2021-09-19 00:42:08 +0700

    updated

--- product/product.js
+++ product/product.js
@@ -16,12 +16,19 @@ router.get("/", function (req, res) {
 });
 
 router.get("/:product_cat", function (req, res) {
+  console.log("Name: ", req.params.product_cat);
   var all_product = JSON.parse(fs.readFileSync("./product/data.json", "utf8"));
+  var list_name = JSON.parse(fs.readFileSync("./product/list.json", "utf8"));
   for (var product in all_product.subfolder) {
     if (all_product.subfolder[product].url === req.params.product_cat) {
-      res.render("pages/product-by-cat.ejs", {
-        productcat: all_product.subfolder[product],
-      });
+      try {
+        res.render("pages/product_by_cats.ejs", {
+          productcat: all_product.subfolder[product],
+          list: list_name,
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 });
--- thaictplas.js
+++ thaictplas.js
@@ -1,41 +1,44 @@
-var express = require('express')
-var app = express()
-var path = require('path')
-var http = require('http')
-var https = require('https')
-var url = require('url')
-var fs = require('fs')
-var product = require('./product/product')
-app.use(express.static(path.join(__dirname, 'public')));
-app.enable('trust proxy')
+var express = require("express");
+var app = express();
+var path = require("path");
+var http = require("http");
+var https = require("https");
+var url = require("url");
+var fs = require("fs");
+var product = require("./product/product");
+app.use(express.static(path.join(__dirname, "public")));
+app.enable("trust proxy");
 var hostname = "";
 app.use(function (req, res, next) {
-    if (req.secure) {
-        hostname = req.headers.host;
-        console.log("hostname = " + hostname);
-        next();
-    } else {
-        console.log(req.headers.host);
-        res.redirect('https://'+req.headers.host+req.url);
-    }
+  if (req.secure) {
+    hostname = req.headers.host;
+    console.log("hostname = " + hostname);
+    next();
+  } else {
+    console.log(req.headers.host);
+    res.redirect("https://" + req.headers.host + req.url);
+  }
 });
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
-    res.render('pages/index.ejs');
-})
-app.get('/aboutus', function(req,res) {
-    res.render('pages/aboutus.ejs');
-})
-app.get('/contactus', function(req,res) {
-    res.render('pages/contactus.ejs');
-})
-app.use('/product', product)
+app.get("/", function (req, res) {
+  res.render("pages/index.ejs");
+});
+app.get("/aboutus", function (req, res) {
+  res.render("pages/aboutus.ejs");
+});
+app.get("/contactus", function (req, res) {
+  res.render("pages/contactus.ejs");
+});
+app.get("/catalogue", function (req, res) {
+  res.render("pages/catalogue.ejs");
+});
+app.use("/product", product);
 
 const options = {
-    cert: fs.readFileSync('./sslcert/fullchain.pem'),
-    key: fs.readFileSync('./sslcert/privkey.pem')
+  cert: fs.readFileSync("./sslcert/fullchain.pem"),
+  key: fs.readFileSync("./sslcert/privkey.pem"),
 };
 
-app.listen(80)
-https.createServer(options, app).listen(443)
+app.listen(80);
+https.createServer(options, app).listen(443);
